34297399bbce49409c6a2c1b9ee83bf664cacad2
Omar <benjami4n@gmail.com>
2024-09-28 07:49:03 +0300

    Added routes Subribstions/Settings/Peers/Statistics

--- front/src/App.js
+++ front/src/App.js
@@ -1,12 +1,42 @@
-import React from "react";
+// src/App.js
+import React, { useState } from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Login from "./components/Login";
+import Home from "./components/Home";
 import SubscribersTable from "./components/SubscribersTable";
+import ModInfo from "./components/ModInfo";
+import Peers from "./components/Peers";
+import DefaultPage from "./components/DefaultPage";
 
-function App() {
+const App = () => {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  const handleLogin = () => {
+    setIsLoggedIn(true);
+  };
+
   return (
-    <div className="App">
-      <SubscribersTable />
-    </div>
+    <Router>
+      <Routes>
+        {isLoggedIn ? (
+          <>
+            <Route path="/" element={<Home />}>
+              <Route index element={<DefaultPage />} /> {/* Default page */}
+              <Route path="subscribers" element={<SubscribersTable />} />
+              <Route path="settings" element={<ModInfo />} />
+              <Route
+                path="statistics"
+                element={<div>Statistics (Coming Soon)</div>}
+              />
+              <Route path="peers" element={<Peers />} />
+            </Route>
+          </>
+        ) : (
+          <Route path="/" element={<Login onLogin={handleLogin} />} />
+        )}
+      </Routes>
+    </Router>
   );
-}
+};
 
 export default App;
